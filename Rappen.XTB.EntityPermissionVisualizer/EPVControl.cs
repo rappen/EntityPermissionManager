b86422958482a3216cdcbf4466a993c541651547
Jonas Rapp <jonas@jonasr.app>
2021-01-08 17:23:48 +0100

    Some error handling and refactoring

--- Rappen.XTB.EntityPermissionVisualizer/EPVControl.cs
+++ Rappen.XTB.EntityPermissionVisualizer/EPVControl.cs
@@ -31,28 +31,6 @@ namespace Rappen.XTB.EPV
 
         #region Public Methods
 
-        public static string GetFullWebApplicationUrl(ConnectionDetail connectionDetail)
-        {
-            var url = connectionDetail.WebApplicationUrl;
-            if (string.IsNullOrEmpty(url))
-            {
-                url = connectionDetail.ServerName;
-            }
-            if (!url.ToLower().StartsWith("http"))
-            {
-                url = string.Concat("http://", url);
-            }
-            var uri = new Uri(url);
-            if (!uri.Host.EndsWith(".dynamics.com"))
-            {
-                if (string.IsNullOrEmpty(uri.AbsolutePath.Trim('/')))
-                {
-                    uri = new Uri(uri, connectionDetail.Organization);
-                }
-            }
-            return uri.ToString();
-        }
-
         public override void ClosingPlugin(PluginCloseInfo info)
         {
             base.ClosingPlugin(info);
--- Rappen.XTB.EntityPermissionVisualizer/EPVcode.cs
+++ Rappen.XTB.EntityPermissionVisualizer/EPVcode.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.Xrm.Sdk;
+﻿using McTools.Xrm.Connection;
+using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Metadata;
 using Microsoft.Xrm.Sdk.Query;
+using Rappen.XTB.Helpers;
 using Rappen.XTB.Helpers.ControlItems;
 using Rappen.XTB.Helpers.Controls;
 using Rappen.XTB.Helpers.Extensions;
@@ -29,6 +31,28 @@ namespace Rappen.XTB.EPV
 
         #region Private Methods
 
+        private static string GetFullWebApplicationUrl(ConnectionDetail connectionDetail)
+        {
+            var url = connectionDetail.WebApplicationUrl;
+            if (string.IsNullOrEmpty(url))
+            {
+                url = connectionDetail.ServerName;
+            }
+            if (!url.ToLower().StartsWith("http"))
+            {
+                url = string.Concat("http://", url);
+            }
+            var uri = new Uri(url);
+            if (!uri.Host.EndsWith(".dynamics.com"))
+            {
+                if (string.IsNullOrEmpty(uri.AbsolutePath.Trim('/')))
+                {
+                    uri = new Uri(uri, connectionDetail.Organization);
+                }
+            }
+            return uri.ToString();
+        }
+
         private void DeletePermission(TreeNode node)
         {
             if (!(node.Tag is EntityItem permission))
@@ -52,76 +76,6 @@ namespace Rappen.XTB.EPV
                     permissions = permissions.Where(p => p != permission.Entity);
                 })
             });
-        }
-
-        private static string GetDeepLink(string webappurl, string entity, Guid recordid, Guid viewid, NameValueCollection extraqs)
-        {
-            if (string.IsNullOrEmpty(entity))
-            {
-                return string.Empty;
-            }
-            var uri = new UriBuilder(webappurl);
-            uri.Path = "main.aspx";
-            var query = HttpUtility.ParseQueryString(uri.Query);
-            if (!string.IsNullOrWhiteSpace(entity))
-            {
-                query["etn"] = entity;
-            }
-            if (!viewid.Equals(Guid.Empty))
-            {
-                query["pagetype"] = "entitylist";
-                query["id"] = viewid.ToString();
-            }
-            else
-            {
-                query["pagetype"] = "entityrecord";
-                if (!recordid.Equals(Guid.Empty))
-                {
-                    query["id"] = recordid.ToString();
-                }
-            }
-            if (extraqs != null)
-            {
-                var eq = extraqs.AllKeys.Select(k => k + "=" + HttpUtility.UrlEncode(extraqs[k]));
-                var extraquerystring = string.Join("&", eq);
-                query["extraqs"] = extraquerystring;
-            }
-            uri.Query = query.ToString();
-            return uri.ToString();
-        }
-
-        private static string GetEntityUrl(string webappurl, Entity entity)
-        {
-            var entref = entity.ToEntityReference();
-            switch (entref.LogicalName)
-            {
-                case "activitypointer":
-                    if (!entity.Contains("activitytypecode"))
-                    {
-                        MessageBox.Show("To open records of type activitypointer, attribute 'activitytypecode' must be included in the query.", "Open Record", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                        entref.LogicalName = string.Empty;
-                    }
-                    else
-                    {
-                        entref.LogicalName = entity["activitytypecode"].ToString();
-                    }
-                    break;
-
-                case "activityparty":
-                    if (!entity.Contains("partyid"))
-                    {
-                        MessageBox.Show("To open records of type activityparty, attribute 'partyid' must be included in the query.", "Open Record", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                        entref.LogicalName = string.Empty;
-                    }
-                    else
-                    {
-                        var party = (EntityReference)entity["partyid"];
-                        entref.LogicalName = party.LogicalName;
-                        entref.Id = party.Id;
-                    }
-                    break;
-            }
-            return GetDeepLink(webappurl, entref.LogicalName, entref.Id, Guid.Empty, null);
         }
 
         private void GetChildNodeDetails(TreeNodeCollection nodes)
@@ -160,25 +114,28 @@ namespace Rappen.XTB.EPV
 
         private List<TreeNode> GetChildPermissions(TreeNode parentnode)
         {
-            IEnumerable<Entity> childpermissions = null;
-            if (parentnode == null)
-            {   // All but parental permissions
-                childpermissions = permissions.Where(p => p.TryGetAttributeValue("adx_scope", out OptionSetValue scope) ? scope.Value != 756150003 : true);
-            }
-            else if (parentnode.Tag is EntityItem parentitem)
+            var childnodes = new List<TreeNode>();
+            if (permissions != null)
             {
-                childpermissions = permissions.Where(p => p.TryGetAttributeValue("adx_parententitypermission", out EntityReference parent) ? parent.Id.Equals(parentitem.Entity.Id) : false);
+                IEnumerable<Entity> childpermissions = null;
+                if (parentnode == null)
+                {   // All but parental permissions
+                    childpermissions = permissions.Where(p => p.TryGetAttributeValue(EntitypeRmission.Scope, out OptionSetValue scope) ? scope.Value != (int)EntitypeRmission.Scope_OptionSet.Overordnad : true);
+                }
+                else if (parentnode.Tag is EntityItem parentitem)
+                {
+                    childpermissions = permissions.Where(p => p.TryGetAttributeValue(EntitypeRmission.ParentEntitypeRmission, out EntityReference parent) ? parent.Id.Equals(parentitem.Entity.Id) : false);
+                }
+                if (childpermissions.Count() > 0)
+                {
+                    var childitems = childpermissions
+                        .Select(e => new PermissionItem(e, rbTreeRels.Checked, Service))
+                        .OrderBy(p => p.OrderBy);
+                    childnodes = childitems
+                        .Select(p => p.ToNode()).ToList();
+                    childnodes.ForEach(c => c.Nodes.AddRange(GetChildPermissions(c).ToArray()));
+                }
             }
-            else
-            {
-                return new List<TreeNode>();
-            }
-            var childitems = childpermissions
-                .Select(e => new PermissionItem(e, rbTreeRels.Checked, Service))
-                .OrderBy(p => p.OrderBy);
-            var childnodes = childitems
-                .Select(p => p.ToNode()).ToList();
-            childnodes.ForEach(c => c.Nodes.AddRange(GetChildPermissions(c).ToArray()));
             return childnodes;
         }
 
@@ -200,14 +157,13 @@ namespace Rappen.XTB.EPV
             }
             return null;
         }
-
         private string GetPermissionEntityName(EntityItem permissionitem)
         {
             if (permissionitem == null)
             {
                 return string.Empty;
             }
-            if (permissionitem.Entity.TryGetAttributeValue("adx_entitylogicalname", out string itementity))
+            if (permissionitem.Entity.TryGetAttributeValue(EntitypeRmission.EntityLogicalName, out string itementity))
             {
                 if (GetEntityMetadataItem(itementity) is EntityMetadataItem emdi)
                 {
@@ -319,7 +275,7 @@ namespace Rappen.XTB.EPV
                 extraqs.Add(EntitypeRmission.ParentEntitypeRmission, parent.Id.ToString());
                 extraqs.Add(EntitypeRmission.ParentEntitypeRmission + "name", txtItemName.Text);
             }
-            var url = GetDeepLink(GetFullWebApplicationUrl(ConnectionDetail), EntitypeRmission.EntityName, Guid.Empty, Guid.Empty, extraqs);
+            var url = Utils.GetDeepLink(GetFullWebApplicationUrl(ConnectionDetail), EntitypeRmission.EntityName, Guid.Empty, Guid.Empty, extraqs);
             if (!string.IsNullOrEmpty(url))
             {
                 Process.Start(url);
@@ -332,7 +288,7 @@ namespace Rappen.XTB.EPV
             {
                 return;
             }
-            string url = GetEntityUrl(GetFullWebApplicationUrl(ConnectionDetail), entity);
+            string url = Utils.GetDeepLink(GetFullWebApplicationUrl(ConnectionDetail), entity.LogicalName, entity.Id, Guid.Empty, null);
             if (!string.IsNullOrEmpty(url))
             {
                 Process.Start(url);
